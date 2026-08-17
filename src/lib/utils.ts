dc67c80b2e6cf607bd238e81b70d3b92ce078cbd
Kimberly <kimberlykeisha9@gmail.com>
2026-08-17 16:45:46 +0300

    fix utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,5 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 export function getAssetPath(path: string) {
   const cleanPath = path.startsWith("/") ? path : `/${path}`;
   return cleanPath;
 }
+
